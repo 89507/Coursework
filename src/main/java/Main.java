5809b449cd5111da8a373c8707b5473eee59909a
89507 <89507@farnborough.ac.uk>
2019-07-01 11:25:41 +0100

    moved user methods to USerController

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -49,44 +49,7 @@ public class Main {
             System.out.println("Database disconnection error: " + exception.getMessage());
         }
     }
-    // This method prints out the contents of users from my SQL table
-    private static void getallUsers(){
 
-        try {
-
-            PreparedStatement ps = db.prepareStatement("SELECT * FROM Users");
-
-            ResultSet results = ps.executeQuery();
-            while (results.next()) {
-                int userID = results.getInt(1);
-                String username= results.getString(2);
-                String score = results.getString(3);
-                System.out.println(userID + " " + username + " " + score);
-            }
-
-        } catch (Exception exception) {
-            System.out.println("Database error: " + exception.getMessage());
-        }
-
-    }
-    // this method adds a new user
-    private static void adduser(String username, String DOB){
-
-        try {
-
-            PreparedStatement ps = db.prepareStatement("INSERT INTO Users (username, DOB) VALUES (?, ?)");
-
-            ps.setString(1, username);
-            ps.setString(2, DOB);
-
-            ps.executeUpdate();
-
-        } catch (Exception exception) {
-            System.out.println("Database error: " + exception.getMessage());
-        }
-
-
-    }
 
 }
 
