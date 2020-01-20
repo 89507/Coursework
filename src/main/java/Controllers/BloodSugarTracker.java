fe33644cb03df5e2fcccd3fe8594fd3e36513612
89507 <89507@farnborugh.ac.uk>
2020-01-20 12:16:43 +0000

    JSOn phase 2

--- src/main/java/Controllers/BloodSugarTracker.java
+++ src/main/java/Controllers/BloodSugarTracker.java
@@ -1,13 +1,84 @@
 package Controllers;
 
 import Sever.Main;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import javax.swing.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class BloodSugarTracker {
 
 
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public static String getallBloodSugarTracker() {
+        System.out.println("BloodSugarTracker/list");
+        JSONArray list = new JSONArray();
+
+        try {
+//      This is a Crud statement to Read
+            PreparedStatement ps = Main.db.prepareStatement("SELECT SugarID, UserID, Date, Time, SugarLevel FROM BloodSugarTracker");
+
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+
+                JSONObject item = new JSONObject();
+                item.put("SugarID", results.getInt(1));
+                item.put("UserID", results.getInt(2));
+                item.put("Date", results.getString(3));
+                item.put("Time", results.getString(4));
+                item.put("SugarLevel", results.getInt(5));
+                list.add(item);
+
+            }
+            return list.toString();
+
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
     public static void getallBloodSugarTracker() {
         System.out.println("BloodSugarTracker/list");
 
@@ -36,10 +107,52 @@ public class BloodSugarTracker {
 
         }
     }
+*/
+
+    @POST
+    @Path("Add")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String AddBloodSugarTracker(
+            @FormDataParam("UserID") Integer UserID, @FormDataParam("Date") String Date, @FormDataParam("Time") String Time, @FormDataParam("SugarLevel") Integer SugarLevel) {
+
+        try {
+            if (UserID == null || Date == null || Time == null || SugarLevel == null) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
+            System.out.println("add/new UserID=" + UserID);
+
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (UserID, Date, Time, SugarLevel) VALUES ( ?, ?, ?, ?,)");
+            ps.setInt(1, UserID);
+            ps.setString(2, Date);
+            ps.setString(3, Time);
+            ps.setInt(4, SugarLevel);
+            ps.execute();
+
+            return "{\"status\": \"OK\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+    }
 
 
 
-    // This is a CRUD create statement
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*   // This is a CRUD create statement
     public static void addBloodSugarTracker(Integer SugarID, Integer UserID, String Date, String Time, Integer SugarLevel) {
 
         try {
@@ -56,11 +169,42 @@ public class BloodSugarTracker {
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
         }
+    }*/
+
+
+    // this updates the bloodsugarData data
+    @POST
+    @Path("update")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateBloodSugarTracker(
+            @FormDataParam("UserID") Integer UserID, @FormDataParam("Date") String Date, @FormDataParam("Time") String Time, @FormDataParam("SugarLevel") Integer SugarLevel) {
+        try {
+            if (UserID == null || Date == null || Time == null || SugarLevel == null) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
+            System.out.println("Users/update UserID=" + UserID);
+
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE Users SET UserID = ?, Date = ?, Time = ?, SugarLevel = ? WHERE UserID = ?");
+            ps.setInt(1, UserID);
+            ps.setString(2, Date);
+            ps.setString(3, Time);
+            ps.setInt(4, SugarLevel);
+            ps.execute();
+            return "{\"status\": \"OK\"}";
+
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to update item, please see server console for more info.\"}";
+        }
     }
 
 
 
-    // This a update crud statement
+
+
+
+   /* // This a update crud statement
     public static void updateBloodSugarTracker(Integer SugarID, Integer UserID, String Date, String Time, Integer SugarLevel) {
 
 
@@ -81,10 +225,64 @@ public class BloodSugarTracker {
         }
 
 
+    }*/
+
+
+    @POST
+    @Path("Delete")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String DeleteBloodSugarTracker(@FormDataParam("UserID") Integer UserID) {
+
+        try {
+            if (UserID == null) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
+            System.out.println("thing/delete UserID=" + UserID);
+
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Users WHERE UserID = ?");
+
+            ps.setInt(1, UserID);
+
+            ps.execute();
+
+            return "{\"status\": \"OK\"}";
+
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to delete item, please see server console for more info.\"}";
+
+        }
     }
+}
 
 
-    public static void deleteBloodSugarTracker(int SugarID){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* public static void deleteBloodSugarTracker(int SugarID){
         try{
             PreparedStatement ps = Main.db.prepareStatement("DELETE FROM BloodSugarTracker WHERE SugarID = ?");
 
@@ -97,4 +295,4 @@ public class BloodSugarTracker {
     }
 
 
-}
+}*/
--- src/main/java/Controllers/MealsController.java
+++ src/main/java/Controllers/MealsController.java
@@ -1,13 +1,71 @@
 package Controllers;
 
 import Sever.Main;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class MealsController {
 
-    public static void getallMeals() {
+
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public static String getallMeals(){
+        System.out.println("Meals/list");
+        JSONArray list = new JSONArray();
+
+        try {
+//      This is a Crud statement to Read
+            PreparedStatement ps = Main.db.prepareStatement("SELECT MealID, Name, PortionSize, Carbs, Custom FROM Meals");
+
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+
+                JSONObject item = new JSONObject();
+                item.put("MealID", results.getInt(1));
+                item.put("Name", results.getString(2));
+                item.put("PortionSize", results.getInt(3));
+                item.put("Carbs", results.getInt(4));
+                item.put("Custom", results.getBoolean(5));
+                list.add(item);
+
+            }
+            return list.toString();
+
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* public static void getallMeals() {
         System.out.println("Meals/list");
 
         try {
@@ -34,10 +92,51 @@ public class MealsController {
             System.out.println("Database error: " + exception.getMessage());
 
         }
+    }*/
+
+    @POST
+    @Path("Add")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String AddMeals(
+            @FormDataParam("Name") String Name, @FormDataParam("PortionSize") Integer PortionSize, @FormDataParam("Carbs") Integer Carbs, @FormDataParam("Custom")Boolean Custom){
+
+        try{
+            if ( Name == null || PortionSize == null || Carbs == null || Custom == null ) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
+            System.out.println("add/new Name =" + Name);
+
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Meals(Name, Portionsize, Carbs, Custom) VALUES (?, ?, ?, ?)");
+            ps.setString(1, Name);
+            ps.setInt(2, PortionSize);
+            ps.setInt(3, Carbs);
+            ps.setBoolean(4, Custom);
+            ps.execute();
+            return "{\"status\": \"OK\"}";
+        } catch (Exception exception){
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
     }
 
 
-    // This is a CRUD create statement
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* // This is a CRUD create statement
     public static void adduser(Integer userID, String username, String DOB, String email, String gender, String password) {
 
         try {
@@ -56,13 +155,63 @@ public class MealsController {
             System.out.println("Database error: " + exception.getMessage());
         }
     }
+*/
+
+    @POST
+    @Path("update")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateMeals(
+            @FormDataParam("MealId") Integer MealID, @FormDataParam("Name")String Name, @FormDataParam("Portionsize") Integer PortionSize, @FormDataParam("Carbs") Integer Carbs, @FormDataParam("Custom") Boolean Custom) {
+        try {
+            if (MealID == null || Name == null || PortionSize == null || Carbs == null || Custom == null) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
+            System.out.println("Meals/update Name=" + Name);
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE Meals SET MealID = ?, Name = ?, Portionsize = ?, Carbs = ?, Custom = ? WHERE MealID = ?");
+            ps.setInt(1, MealID);
+            ps.setString(2, Name);
+            ps.setInt(3, PortionSize);
+            ps.setInt(4, Carbs);
+            ps.setBoolean(5, Custom);
+            ps.execute();
+            return "{\"status\": \"OK\"}";
+
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to update item, please see server console for more info.\"}";
+        }
+
+    }
 
 
 
+    @POST
+    @Path("Delete")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String DeleteMeals(@FormDataParam("MealID") Integer MealID) {
 
 
+        try {
+            if(MealID == null) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
+            System.out.println("Meal/delete UserID=" + MealID);
 
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Meals WHERE MealID = ?");
 
+            ps.setInt(1, MealID);
+
+            ps.execute();
+
+            return "{\"status\": \"OK\"}";
+
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to delete item, please see server console for more info.\"}";
+        }
+    }
 
 
 
